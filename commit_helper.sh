6798517931d0d0f23aa0e39024a8e48019cea4b3
Matthias Braun <matthias.braun@kit.edu>
2013-01-29 17:47:22 +0100

    add commit helper script

---
+++ commit_helper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Takes a newly generated fail_expectations and attempts to merge/commit it into
+# the repository
+set -eu
+
+new_expectations="$1"
+file="$2"
+
+if diff "${new_expectations}" "${file}" >& /dev/null; then
+	echo "No differences"
+	exit 0
+fi
+
+cp "${new_expectations}" "${file}"
+git add "${file}"
+git commit -m "buildbot update"
+while ! git push; do
+	git pull --rebase
+done
