7b04c4045ed78515844e7a939045aeb29a9dcb74
Andreas Zwinkau <zwinkau@kit.edu>
2013-11-14 12:33:40 +0100

    ensure 'origin' remote is set

--- commit_helper.sh
+++ commit_helper.sh
@@ -7,16 +7,22 @@ set -euv
 new_expectations="$1"
 file="$2"
 
+# check if push is necessary
 if diff "${new_expectations}" "${file}" >& /dev/null; then
 	echo "No differences"
 	exit 0
 fi
 
+# ensure 'origin' remote is set
+git remote rm origin || true
 git remote add origin /ben/local/GIT/public/firm-testresults
 
+# locally commit changes
 cp "${new_expectations}" "${file}"
 git add "${file}"
 git commit -m "buildbot update" --author "buildbot <firm@ipd.info.uni-karlsruhe.de>"
+
+# publish changes
 while ! git push origin master -u; do
 	git fetch origin
 	git rebase master -s theirs
